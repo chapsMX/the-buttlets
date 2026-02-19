20eb515225ca4f19906452e53c3cafa53565f438
chapsMX <carlos@alcocersola.com>
2026-02-19 12:40:26 -0600

    neynarNotis

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  transpilePackages: ["@neynar/react", "@pigment-css/react"],
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
